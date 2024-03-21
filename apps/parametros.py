9ba05167b7a616a3109334221262e1e371816fba
Breno Fernandes <bsfernandes98@hotmail.com>
2024-03-21 15:22:54 -0300

    up

--- apps/parametros.py
+++ apps/parametros.py
@@ -1,7 +1,7 @@
 corporate_group_id = 1  # Corporate Group Id da Migração = 1 - ONODERA PORTUGAL,
 codigofilial_sw = 2  # Braga
 nomefilial_sw = "Braga"
-codigo_gester = 12
+codigo_gester = 2
 location_country_id = 164  # Portugal
 default_sales_channel_id = 1  # Canal de venda da própria unidade
 update_if_exists = True
